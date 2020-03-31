4e356a912d0e35b10093b4a824d9f6c6eaa06c8c
Neeraj Mittal <5064901+neermitt@users.noreply.github.com>
2020-03-31 14:22:20 +0530

    added python language support

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ RUN mvn -f /usr/src/app/pom.xml clean package
 FROM openjdk:15-jdk-alpine
 COPY  --from=build /usr/src/app/target/zeebe-script-worker-0.7.0.jar app.jar
 EXPOSE 8080
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-Dpython.import.site=false","-jar","/app.jar"]
--- src/main/java/io/zeebe/script/ScriptEvaluator.java
+++ src/main/java/io/zeebe/script/ScriptEvaluator.java
@@ -15,44 +15,37 @@
  */
 package io.zeebe.script;
 
-import org.python.core.Options;
-
+import javax.script.*;
 import java.util.HashMap;
 import java.util.Map;
-import javax.script.Bindings;
-import javax.script.ScriptContext;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 
 public class ScriptEvaluator {
 
-  private final ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+    private final ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
 
-  private final Map<String, ScriptEngine> cachedScriptEngines = new HashMap<>();
+    private final Map<String, ScriptEngine> cachedScriptEngines = new HashMap<>();
 
-  public Object evaluate(String language, String script, Map<String, Object> variables) {
-      Options.importSite = false;
+    public Object evaluate(String language, String script, Map<String, Object> variables) {
 
-    final ScriptEngine scriptEngine =
-        cachedScriptEngines.computeIfAbsent(language, scriptEngineManager::getEngineByName);
+        final ScriptEngine scriptEngine =
+                cachedScriptEngines.computeIfAbsent(language, scriptEngineManager::getEngineByName);
 
-    if (scriptEngine == null) {
-      final String msg = String.format("No script engine found with name '%s'", language);
-      throw new RuntimeException(msg);
+        if (scriptEngine == null) {
+            final String msg = String.format("No script engine found with name '%s'", language);
+            throw new RuntimeException(msg);
+        }
+
+        final ScriptContext context = scriptEngine.getContext();
+        final Bindings bindings = context.getBindings(ScriptContext.ENGINE_SCOPE);
+        bindings.putAll(variables);
+
+        try {
+
+            return scriptEngine.eval(script, context);
+
+        } catch (ScriptException e) {
+            final String msg = String.format("Failed to evaluate script '%s' (%s)", script, language);
+            throw new RuntimeException(msg, e);
+        }
     }
-
-    final ScriptContext context = scriptEngine.getContext();
-    final Bindings bindings = context.getBindings(ScriptContext.ENGINE_SCOPE);
-    bindings.putAll(variables);
-
-    try {
-
-      return scriptEngine.eval(script, context);
-
-    } catch (ScriptException e) {
-      final String msg = String.format("Failed to evaluate script '%s' (%s)", script, language);
-      throw new RuntimeException(msg, e);
-    }
-  }
 }
